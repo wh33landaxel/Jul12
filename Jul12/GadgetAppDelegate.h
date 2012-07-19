b630daaa0394d19c99ba3f990c5e845a1a73e65c
wh33landaxel <xlnunez317@gmail.com>
2012-07-19 19:20:39 -0400

    Initial Commit

---
+++ Jul12/GadgetAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GadgetAppDelegate.h
+//  Jul12
+//
+//  Created by Axel Nunez on 7/19/12.
+//  Copyright (c) 2012 CUNY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GadgetAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jul12/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jul12
+//
+//  Created by Axel Nunez on 7/19/12.
+//  Copyright (c) 2012 CUNY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GadgetAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GadgetAppDelegate class]));
+    }
+}
